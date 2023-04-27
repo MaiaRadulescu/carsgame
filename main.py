f4fbe8cce00d028bce9a81592585d480507d8d83
Maia Radulescu <maiateorad@gmail.com>
2023-04-27 13:43:01 +0300

    import scale_images from utils

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 import pygame
 import time
 import math
-
-
-def scale_image(img, factor):
-    size = round(img.get_width() * factor), round(img.get_height() * factor)
-    return pygame.transform.scale(img, size)
-
-
-def blit_roatate_center(win, image, top_left, angle):
-    rotated_image = pygame.transform.rotate(image, angle)
-    new_rect = rotated_image.get_rect(center=image.get_rect(topleft=top_left).center)
-    win.blit(rotated_image, new_rect.topleft)
-
+from utils import scale_image, blit_rotate_center
 
 GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
 
 TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
-FINISH = pygame.image.load("imgs/finish.png")
 
-RED_CAR = scale_image(pygame.image.load("imgs/red-car.png."), 0.55)
-GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png."), 0.55)
+RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
+GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -31,7 +19,6 @@ FPS = 60
 
 
 class AbstractCar:
-
     def __init__(self, max_vel, rotation_vel):
         self.img = self.IMG
         self.max_vel = max_vel
@@ -48,7 +35,7 @@ class AbstractCar:
             self.angle -= self.rotation_vel
 
     def draw(self, win):
-        blit_roatate_center(win, self.img, (self.x, self.y), self.angle)
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
 
     def move_forward(self):
         self.vel = min(self.vel + self.acceleration, self.max_vel)
@@ -57,14 +44,15 @@ class AbstractCar:
     def move(self):
         radians = math.radians(self.angle)
         vertical = math.cos(radians) * self.vel
-        horizontal = math.sin(radians)
+        horizontal = math.sin(radians) * self.vel
 
-        self.y += vertical
+        self.y -= vertical
         self.x -= horizontal
 
     def reduce_speed(self):
         self.vel = max(self.vel - self.acceleration / 2, 0)
         self.move()
+
 
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
@@ -83,15 +71,11 @@ run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
 player_car = PlayerCar(4, 4)
+
 while run:
     clock.tick(FPS)
 
-    WIN.blit(GRASS, (0, 0))
-    WIN.blit(TRACK, (0, 0))
-    WIN.blit(RED_CAR, (0, 0))
-
     draw(WIN, images, player_car)
-    pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -100,15 +84,17 @@ while run:
 
     keys = pygame.key.get_pressed()
     moved = False
+
     if keys[pygame.K_a]:
         player_car.rotate(left=True)
-
     if keys[pygame.K_d]:
         player_car.rotate(right=True)
-
     if keys[pygame.K_w]:
         moved = True
         player_car.move_forward()
+
     if not moved:
         player_car.reduce_speed()
+
+
 pygame.quit()
